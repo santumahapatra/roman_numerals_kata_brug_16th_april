f570b2286ac414ca086c14f04f76be3f7628184e
Santu Mahapatra <santumahapatra@gmail.com>
2016-04-18 10:22:21 +0530

    All basic tests passing

---
+++ roman_numerals.rb
@@ -0,0 +1,56 @@
+class Fixnum
+  ROMAN_NUMERAL = {
+    1 =>"I",
+    4 => "IV",
+    5 => "V",
+    9 => "IX",
+    10 => "X",
+    40 => "XL",
+    50 => "L",
+    90 => "XC",
+    100 => "C",
+    400 => "CD",
+    500 => "D",
+    900 => "CM",
+    1000 => "M"
+  }
+
+  def to_roman
+
+    puts "******************* NUMBER="
+    puts self
+
+    if ROMAN_NUMERAL.keys.include?(self)
+      return ROMAN_NUMERAL[self]
+    end
+
+    roman  = ""
+    divisor = closest_roman
+    quotient = self / divisor
+
+    the_cut = quotient * divisor
+
+    quotient.times do
+      roman.concat(ROMAN_NUMERAL[divisor])
+    end
+
+    remainder = self - (the_cut)
+
+    roman.concat(remainder.to_roman) unless remainder == 0
+
+
+    puts roman
+    puts "*******************"
+    roman
+  end
+
+  def closest_roman
+    divisor = 0
+    ROMAN_NUMERAL.keys.each do |key|
+      puts "blah: #{self/key}"
+      divisor = key if self/key > 0
+    end
+    puts "divisor: #{divisor}"
+    divisor
+  end
+end
